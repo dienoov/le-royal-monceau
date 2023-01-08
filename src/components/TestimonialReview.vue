7de3cfb15e56930ac244e476101a87392b33cb4d
dienoov <alvinodien@yahoo.co.id>
2023-01-08 15:32:21 +0700

    Add modal to add review

--- src/components/TestimonialReview.vue
+++ src/components/TestimonialReview.vue
@@ -22,16 +22,109 @@
         </h4>
       </article>
       <button type="button" class="bg-neutral-800 hover:bg-neutral-900 ease duration-150
-          text-neutral-50 w-48 mx-auto block px-4 py-2 mt-4">
+          text-neutral-50 w-48 mx-auto block px-4 py-2 mt-4" @click="this.modal.show">
         Write a Review
       </button>
     </div>
+    <form id="review" tabindex="-1" aria-hidden="true"
+          class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto
+          md:inset-0 h-modal md:h-full" @submit.prevent="submitForm">
+      <div class="relative w-full h-full max-w-2xl md:h-auto">
+        <div class="relative bg-neutral-50 shadow">
+          <h3 class="text-3xl font-serif font-medium text-neutral-900 text-center p-4">
+            Write your review
+          </h3>
+          <div class="p-6 space-y-6">
+            <div class="flex flex-col">
+              <label for="name" class="text-sm font-medium">Name</label>
+              <input type="text" v-model="form.name" class="border px-4 py-2 mt-2
+              focus:border-neutral-900 focus:ring-neutral-900">
+              <p class="text-red-500 text-sm mt-2">{{ errors.name }}</p>
+            </div>
+            <div class="flex flex-col">
+              <label for="name" class="text-sm font-medium">Title</label>
+              <input type="text" v-model="form.title" class="border px-4 py-2 mt-2
+              focus:border-neutral-900 focus:ring-neutral-900">
+              <p class="text-red-500 text-sm mt-2">{{ errors.title }}</p>
+            </div>
+            <div class="flex flex-col mt-4">
+              <label for="message" class="text-sm font-medium">Message</label>
+              <textarea v-model="form.message" class="border px-4 py-2 mt-2
+              focus:border-neutral-900 focus:ring-neutral-900"></textarea>
+              <p class="text-red-500 text-sm mt-2">{{ errors.message }}</p>
+            </div>
+          </div>
+          <div class="flex justify-end items-center p-6 space-x-2">
+            <button type="button" class="bg-neutral-50 hover:bg-neutral-200 ease duration-150
+                text-neutral-900 w-32 block px-4 py-2 mt-4" @click="this.modal.hide">
+              Cancel
+            </button>
+            <button type="submit" class="bg-neutral-800 hover:bg-neutral-900 ease duration-150
+                text-neutral-50 w-32 block px-4 py-2 mt-4">
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
   </article>
 </template>
 
 <script>
+// eslint-disable-next-line no-unused-vars
+import { Modal } from 'flowbite';
+
 export default {
   name: 'TestimonialReview',
+  data() {
+    return {
+      form: {
+        name: '',
+        title: '',
+        message: '',
+      },
+      errors: {
+        name: '',
+        title: '',
+        message: '',
+      },
+      modal: null,
+    };
+  },
+  methods: {
+    validateForm() {
+      this.errors.name = '';
+      this.errors.title = '';
+      this.errors.message = '';
+
+      if (!this.form.title) {
+        this.errors.title = 'Title is required';
+      }
+
+      if (!this.form.name) {
+        this.errors.name = 'Name is required';
+      }
+
+      if (!this.form.message) {
+        this.errors.message = 'Message is required';
+      }
+
+      return this.errors.name === '' && this.errors.title === '' && this.errors.message === '';
+    },
+    submitForm() {
+      if (this.validateForm()) {
+        this.form.name = '';
+        this.form.title = '';
+        this.form.message = '';
+        this.modal.hide();
+      }
+    },
+  },
+  mounted() {
+    this.modal = new Modal(document.getElementById('review'), {
+      backdrop: 'static',
+    });
+  },
 };
 </script>
 
