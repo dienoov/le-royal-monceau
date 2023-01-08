9f9539737d95e4dee31e24dbcb50ac3d1c279b65
dienoov <alvinodien@yahoo.co.id>
2023-01-08 17:02:54 +0700

    Improve Navigation Bar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-between items-center relative h-16">
     <nav class="flex items-center">
-      <button type="button" @click="toggleMenu" class="block w-8 h-8 mt-2 ml-4 lg:hidden">
+      <button type="button" @click="toggleMenu" class="block w-8 h-8 ml-4 lg:hidden">
         <span class="block bg-neutral-900 w-5 h-0.5 relative
             before:content-[''] before:block before:bg-neutral-900
             before:w-8 before:h-0.5 before:absolute before:-top-[.66rem]
@@ -14,21 +14,46 @@
       </a>
       <ul ref="menu" class="absolute top-0 flex flex-col items-center w-full mt-16 bg-neutral-50
           lg:static lg:top-auto lg:flex-row lg:items-start lg:w-auto lg:ml-8 lg:mt-0 lg:pt-2
-          -translate-x-full ease duration-300 lg:translate-x-0">
+          -translate-x-full ease duration-300 lg:translate-x-0 pt-4 lg:pt-2">
         <li class="lg:mx-6">
-          <a href="#" class="block text-sm p-4 w-full lg:p-0">Rooms & Suites</a>
+          <a href="#" class="block text-sm text-neutral-900/80 p-4 w-full lg:p-0
+              hover:text-neutral-900 ease duration-150 after:content-['']
+              after:h-0.5 after:w-0 after:bg-neutral-900 after:block
+              hover:after:w-full after:ease after:duration-150">
+            Rooms & Suites
+          </a>
         </li>
         <li class="lg:mx-6">
-          <a href="#" class="block text-sm p-4 w-full lg:p-0">Dining</a>
+          <a href="#" class="block text-sm text-neutral-900/80 p-4 w-full lg:p-0
+              hover:text-neutral-900 ease duration-150 after:content-['']
+              after:h-0.5 after:w-0 after:bg-neutral-900 after:block
+              hover:after:w-full after:ease after:duration-150">
+            Dining
+          </a>
         </li>
         <li class="lg:mx-6">
-          <a href="#" class="block text-sm p-4 w-full lg:p-0">Events</a>
+          <a href="#" class="block text-sm text-neutral-900/80 p-4 w-full lg:p-0
+              hover:text-neutral-900 ease duration-150 after:content-['']
+              after:h-0.5 after:w-0 after:bg-neutral-900 after:block
+              hover:after:w-full after:ease after:duration-150">
+            Events
+          </a>
         </li>
         <li class="lg:mx-6">
-          <a href="#" class="block text-sm p-4 w-full lg:p-0">Offers</a>
+          <a href="#" class="block text-sm text-neutral-900/80 p-4 w-full lg:p-0
+              hover:text-neutral-900 ease duration-150 after:content-['']
+              after:h-0.5 after:w-0 after:bg-neutral-900 after:block
+              hover:after:w-full after:ease after:duration-150">
+            Offers
+          </a>
         </li>
         <li class="lg:mx-6">
-          <a href="#" class="block text-sm p-4 w-full lg:p-0">Gallery</a>
+          <a href="#" class="block text-sm text-neutral-900/80 p-4 w-full lg:p-0
+              hover:text-neutral-900 ease duration-150 after:content-['']
+              after:h-0.5 after:w-0 after:bg-neutral-900 after:block
+              hover:after:w-full after:ease after:duration-150">
+            Gallery
+          </a>
         </li>
       </ul>
     </nav>
