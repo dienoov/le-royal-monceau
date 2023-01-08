e11f7b6e1c19c7065c5f040c15c4c587ea5e95e6
dienoov <alvinodien@yahoo.co.id>
2023-01-08 08:01:02 +0700

    Add Hero Slider

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import NavigationBar from './components/NavigationBar.vue';
+import HeroSlider from './components/HeroSlider.vue';
 </script>
 
 <template>
   <NavigationBar />
+  <main>
+    <HeroSlider />
+  </main>
 </template>
 
 <style scoped>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,vue}",
+    './index.html',
+    './src/**/*.{js,ts,vue}',
+    './node_modules/flowbite/**/*.js',
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Beatrice', 'sans-serif'],
+      serif: ['Butler', 'serif'],
+      body: ['Beatrice', 'sans-serif'],
+    }
   },
-  plugins: [],
+  plugins: [
+      require('flowbite/plugin'),
+  ],
 }
